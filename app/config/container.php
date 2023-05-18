82cf98bc04b07694091993a4d2c897ae0c107473
Josh Washburn <thewashburns@gmail.com>
2023-05-18 16:12:14 -0500

    Update container.php

--- app/config/container.php
+++ app/config/container.php
@@ -10,8 +10,7 @@ use function DI\string;
 
 return [
     /** Path definitions */
-    # 'base_path' => dirname(__DIR__, 2),
-    'base_path' => dirname(__DIR__, 1),
+    'base_path' => dirname(__DIR__, 2),
     'app_path' => dirname(__DIR__),
     'asset_path' => string('{app_path}/assets'),
     'cache_path' => string('{app_path}/cache'),
